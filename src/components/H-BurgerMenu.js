93a4f4bc19210ba7acf04d65a89b79acc4c60256
silviaespanagil <silviaespanagil@gmail.com>
2021-08-13 16:52:22 +0200

    Changed burger menu to it's own component and add  a closemenu handler

---
+++ src/components/H-BurgerMenu.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+//PACKAGES
+import { slide as Menu } from "react-burger-menu";
+
+const BurgerMenu = () => {
+  const [isClosed, setClosed] = useState();
+
+  const closeMenuHandler = () => {
+    if (isClosed === false) {
+      return setClosed(true);
+    } else {
+      setClosed(false);
+    }
+  };
+  return (
+    <Menu isOpen={isClosed} width={280}>
+      <Link to="/" className="menu__item" onClick={closeMenuHandler}>
+        Home
+      </Link>
+      <Link to="/favoritos" className="menu__item" onClick={closeMenuHandler}>
+        Favorites
+      </Link>
+    </Menu>
+  );
+};
+
+export default BurgerMenu;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,11 @@
-import { Link } from "react-router-dom";
-
 //COMPONENTS
+import BurgerMenu from "./H-BurgerMenu";
 import HorizontalMenu from "./H-HorizontalMenu";
-
-//PACKAGES
-import { slide as Menu } from "react-burger-menu";
 
 const Header = (props) => {
   return (
     <header>
-      <Menu width={280}>
-        <Link to="/" className="menu__item">
-          Home
-        </Link>
-        <Link to="/favoritos">Favorites</Link>
-      </Menu>
+      <BurgerMenu />
       <HorizontalMenu />
     </header>
   );
