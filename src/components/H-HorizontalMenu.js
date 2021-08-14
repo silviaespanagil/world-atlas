e7c8760ea7355b055c7fe9e979faf89fe3ab641c
silviaespanagil <silviaespanagil@gmail.com>
2021-08-14 16:33:18 +0200

    Changed Horizontal Menu classes name because it had a conflict with burgermenu classes

--- src/components/H-HorizontalMenu.js
+++ src/components/H-HorizontalMenu.js
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 const HorizontalMenu = () => {
   return (
-    <div className="menu">
+    <div className="hMenu">
       <Link to="/">
-        <h1 className="menu__title">World Atlas</h1>
+        <h1 className="hMenu__title">World Atlas</h1>
       </Link>
 
       <nav>
-        <ul className="menu__ul">
+        <ul className="hMenu__ul">
           <Link to="/">
-            <li className="menu__item">Home</li>
+            <li className="hMenu__item">Home</li>
           </Link>
           <Link to="/favoritos">
-            <li className="menu__item">Favoritos</li>
+            <li className="hMenu__item">Favoritos</li>
           </Link>
         </ul>
       </nav>
